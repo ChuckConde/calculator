df1dac41d865ea52ff8150cf00f3733e405c1ce6
ChuckConde <chuck.conde@gmail.com>
2021-09-02 20:49:35 -0300

    Adding numbers and clear functionality working. Fix on overflow on screen container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  result = 0;
+  newVal = '';
+
+  public clear() {
+    this.newVal = '';
+    this.result = 0;
+  }
+  public numberFunc(num: any) {
+    if (this.newVal) {
+      this.newVal += num;
+    } else {
+      if (num !== '0') {
+        this.newVal = num;
+      } else {
+        return;
+      }
+    }
+  }
 }
