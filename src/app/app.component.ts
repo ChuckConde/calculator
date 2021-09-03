360388d1743bd7b205da53378cb8aa76ab5e8a58
ChuckConde <chuck.conde@gmail.com>
2021-09-02 21:06:59 -0300

    All basic aritmethic functions working, sum, minus, multiply and divide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ export class AppComponent {
   title = 'calculator';
   result = 0;
   newVal = '';
+  storedResult = '0';
+  storedFunc = '';
+  mathApplied = false;
 
   public clear() {
     this.newVal = '';
@@ -16,13 +19,51 @@ export class AppComponent {
   }
   public numberFunc(num: any) {
     if (this.newVal) {
-      this.newVal += num;
+      if(this.mathApplied) {
+        this.newVal = num;
+        this.mathApplied = false;
+      } else {
+        this.newVal += num;
+      }
     } else {
       if (num !== '0') {
         this.newVal = num;
       } else {
-        return;
+        if(this.storedFunc) {
+          this.newVal = num;
+        } else {
+          return;
+        }
       }
+    }
+  }
+  public arithmeticOperation(t: string) {
+    this.mathApplied = true;
+    this.storedResult = this.newVal;
+    this.storedFunc = t;
+  }
+  public equals() {
+    switch (this.storedFunc) {
+      case 'sum':
+        this.result = parseFloat(this.storedResult) + parseFloat(this.newVal) ;
+        this.newVal = '';
+        this.storedResult = '';
+        break;
+      case 'minus':
+        this.result = parseFloat(this.storedResult) - parseFloat(this.newVal) ;
+        this.newVal = '';
+        this.storedResult = '';
+        break;
+      case 'multiply':
+        this.result = parseFloat(this.storedResult) * parseFloat(this.newVal) ;
+        this.newVal = '';
+        this.storedResult = '';
+        break;
+      case 'divide':
+        this.result = parseFloat(this.storedResult) / parseFloat(this.newVal) ;
+        this.newVal = '';
+        this.storedResult = '';
+        break;
     }
   }
 }
