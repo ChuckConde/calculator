b706c5d83c49b6067c26fdb2cc83988264e68031
ChuckConde <chuck.conde@gmail.com>
2021-09-02 21:21:24 -0300

    inverting number type and percentage working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,6 +42,19 @@ export class AppComponent {
     this.storedResult = this.newVal;
     this.storedFunc = t;
   }
+  public changeSymbol() {
+    if(this.newVal) {
+      if(this.newVal[0] === '-') {
+        this.newVal = this.newVal.substring(1);
+      } else {
+        this.newVal = '-' + this.newVal
+      }
+    } else {
+      if(this.result !== 0) {
+        this.result = this.result * -1;
+      }
+    }
+  }
   public equals() {
     switch (this.storedFunc) {
       case 'sum':
@@ -61,6 +74,12 @@ export class AppComponent {
         break;
       case 'divide':
         this.result = parseFloat(this.storedResult) / parseFloat(this.newVal) ;
+        this.newVal = '';
+        this.storedResult = '';
+        break;
+      case 'percentage':
+        let onePercent = parseFloat(this.newVal) / 100;
+        this.result = parseFloat(this.storedResult) * onePercent;
         this.newVal = '';
         this.storedResult = '';
         break;
